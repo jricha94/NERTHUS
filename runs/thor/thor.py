39e92f09280e439b9b063c680f08de230b4828cb
Jarod Richardson <jricha94@vols.utk.edu>
2021-10-06 17:37:03 -0400

    new runs

--- runs/thor/thor.py
+++ runs/thor/thor.py
@@ -24,6 +24,20 @@ thor.refuel_min = 1e-7
 thor.refuel_max = 1e-5
 
 
-thor.get_refuel_rate(cleanup=False)
-thor.save_refuel()
+#thor.get_refuel_rate(cleanup=False)
+#thor.save_refuel()
+
+thor.read_refuel_if_done()
+
+
+thor.histories = 100000
+
+thor.feedback_path = cwd + '/feedback'
+thor.queue = 'fill'
+thor.ompcores = 8 
+thor.run_feedbacks('fs.tot', True, True)
+thor.run_feedbacks('gr.tot', True, True)
+
+thor.feedback_path = cwd + '/feedback/no_therm_exp'
+thor.run_feedbacks('gr.tot', False, True)
 
