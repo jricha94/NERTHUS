8fbd53a209d1b0b046c78b27389bf2bd8a69fda0
Jarod Richardson <jarod@pop-os.localdomain>
2021-12-20 22:05:54 -0500

    feedback run

--- runs/thor/thor.py
+++ runs/thor/thor.py
@@ -12,8 +12,9 @@ thor.ompcores = 32
 thor.enr_min = 0.15
 thor.enr_max = 0.25
 
-thor.get_enrichment()
-thor.save_enrs()
+#thor.get_enrichment()
+#thor.save_enrs()
+thor.read_enrs_if_done()
 
 thor.refuel_path = cwd + '/refuel'
 thor.refuel_enr = 0.1975
@@ -25,15 +26,20 @@ thor.histories = 10000
 thor.refuel_min = 1e-8
 thor.refuel_max = 1e-4
 
-thor.get_refuel_rate(cleanup=True)
-print("Short refuel done")
-thor.clear_refuel_list()
+#thor.get_refuel_rate(cleanup=True)
+#print("Short refuel done")
+#thor.clear_refuel_list()
 
-thor.ngen = 200
-thor.nskip = 60
-thor.histories = 20000
-thor.refuel_min = thor.conv_rate / 10.0
-thor.refuel_max = thor.conv_rate * 10.0
-thor.get_refuel_rate()
+#thor.ngen = 200
+#thor.nskip = 60
+#thor.histories = 20000
+#thor.refuel_min = thor.conv_rate / 10.0
+#thor.refuel_max = thor.conv_rate * 10.0
+#thor.get_refuel_rate()
 
-thor.save_refuel()
+#thor.save_refuel()
+thor.read_refuel_if_done()
+
+
+thor.get_feedbacks('fs.tot')
+thor.get_feedbacks('gr.tot')
