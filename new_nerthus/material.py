4820d845017281abdb3c862683bafe84c71c9949
Jarod Richardson <jarod@pop-os.localdomain>
2022-07-21 17:04:17 -0400

    small change

--- new_nerthus/material.py
+++ new_nerthus/material.py
@@ -1,4 +1,6 @@
 
+from salts import Salt
+
 class material:
     def __init__(self, name:str, dens:float, lib:str, isotopes:dict, **kwargs) -> None:
         self.name = name
@@ -25,20 +27,50 @@ class material:
 
         return card
 
+steel_description = "SUS316 Stainless Steel"
+steel_isotopes = {
+     6000: [-0.0008, "Natural Carbon"],
+     7014: [-0.001, "N-14"],
+    14000: [-0.0075, "Natural Silicon"],
+    15031: [-0.00045, "P-31"],
+    16000: [-0.0003, "Natural Sulfur"],
+    24000: [-0.17, "Natural Chromium"],
+    25055: [-0.02, "Mn-55"],
+    26000: [-0.65495, "Natural Iron"],
+    28000: [-.12, "Natural Nickel"],
+    42000: [-0.025, "Natural Molybdenum"],
+}
+
+boron_carbide_description = "Boron Carbide (Natural Boron)"
+boron_carbide_isotopes = {
+    5010: [0.16, "B-10"],
+    5011: [0.64, "B-11"],
+    6000: [0.2, "Natural Carbon"],
+}
+
+sheild_description = "Outer Shield (90% Graphite, 10% B4C)"
+sheild_isotopes = {
+    5010: [0.016, "B-10"],
+    5011: [0.064, "B-11"],
+    6000: [0.92, "Natural Carbon"],
+}
+
+
 class materials:
     
-    def __init__(self) -> None:
-        self.steel = material
-        self.graphite
-        pass
+    def __init__(self, fuelsalt=1, refuelsalt=1, enr=0.02, ref_enr=0.2, lib="09c") -> None:
+        self.lib = lib
+        self.steel = material("steel", -7.9, self.lib, steel_isotopes, rgb="222 58 74")
+        self.boron_carbide = material("B4C_natural", -2.52, self.lib, boron_carbide_isotopes, rgb="43 77 227")
+        self.shield = material("Shield", 1, self.lib, sheild_isotopes, rbg="99 73 214")
+        self.fuelsalt = Salt(fuelsalt, enr)
+        self.refuelsalt = Salt(refuelsalt, ref_enr)
 
 
 
 if __name__ == '__main__':
-    isos = {1001: -0.1, 1002: -0.5, 92235: 0.4}
-    test = material("hello", 7.1235233123423123, "09c", isos, vol = 100, burn = 1)
-    print(test.card("Yes"))
-    isos2 = {1001: [-0.1, "H-1"], 92235: [-0.5, "U-235"], 92239: [-0.4, "Pu-239"]}
+    test = materials()
+    print(test.steel.card(steel_description))
+    print(test.shield.card())
+    print(test.boron_carbide.card())
 
-    test2 = material("graphite", 2.234, "06c", isos2, rgb = "1 99 256", vol = 13670000, burn = 1)
-    print(test2.card("This is a description"))
